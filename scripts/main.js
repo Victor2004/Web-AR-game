e53666b5a88bcf93d03d6e5c89e474a7dfdc5e34
Victor <victorp2908@gmail.com>
2024-03-12 23:10:24 +0300

    Игра по геолокации

--- scripts/main.js
+++ scripts/main.js
@@ -8,14 +8,52 @@ AFRAME.registerComponent('markerhandler', {
     }
 });
 
-window.onload = function () {
+function descriptionButton() {
+    var description = document.querySelector('.description-window');
     document.querySelector(".description-button").addEventListener("click", function () {
-        var description = document.querySelector('.description-window');
-        description.style.visibility = 'visible';
+        if (description.style.visibility == 'hidden') {
+            description.style.visibility = 'visible';
+        } else {
+            description.style.visibility = 'hidden';
+        }
     });
+}
 
+function buttonCloseDescription() {
     document.querySelector(".btn-close-description").addEventListener("click", function () {
         var button = document.querySelector('.description-window');
         button.style.visibility = 'hidden';
     });
+}
+
+// Функция для получения текущих координат
+function getCurrentLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Геолокация не поддерживается вашим браузером.");
+    }
+}
+
+// Функция для отображения координат
+function showPosition(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    // Вывод координат
+    console.log("Ваши текущие координаты:");
+    console.log("Широта:", latitude);
+    console.log("Долгота:", longitude);
+    alert("Широта: " + latitude + " Долгота: " + longitude);
+
+    // Замена координат в коде
+    var code = document.getElementById("your-coordinates");
+    code.innerHTML = code.innerHTML.replace("<your-lat>", latitude).replace("<your-loon>", longitude);
+}
+
+window.onload = function () {
+    descriptionButton();
+    buttonCloseDescription();
+
 };
+getCurrentLocation();
