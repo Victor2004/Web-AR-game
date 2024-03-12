ef92ab341bd421cb2b4781878641f2d024e44de1
Victor <victorp2908@gmail.com>
2024-03-12 21:29:20 +0300

    Исправление кнопок

--- scripts/main.js
+++ scripts/main.js
@@ -9,19 +9,13 @@ AFRAME.registerComponent('markerhandler', {
 });
 
 window.onload = function () {
-    document
-        .querySelector(".description-button")
-        .addEventListener("click", function () {
-            var description = document.querySelector('.description-window');
-            description.style.visibility = 'visible';
-        });
-};
+    document.querySelector(".description-button").addEventListener("click", function () {
+        var description = document.querySelector('.description-window');
+        description.style.visibility = 'visible';
+    });
 
-window.onload = function () {
-    document
-        .querySelector(".btn-close-description")
-        .addEventListener("click", function () {
-            var button = document.querySelector('.description-window');
-            button.style.visibility = 'hidden';
-        });
+    document.querySelector(".btn-close-description").addEventListener("click", function () {
+        var button = document.querySelector('.description-window');
+        button.style.visibility = 'hidden';
+    });
 };
